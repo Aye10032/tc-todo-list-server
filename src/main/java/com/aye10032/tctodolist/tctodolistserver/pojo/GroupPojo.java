79fc86c0835c3727748247b1fb39858216710412
Aye10032 <aye10032@gmail.com>
2022-02-09 21:49:03 +0800

    组实体类

---
+++ src/main/java/com/aye10032/tctodolist/tctodolistserver/pojo/GroupPojo.java
@@ -0,0 +1,34 @@
+package com.aye10032.tctodolist.tctodolistserver.pojo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @program: tc-todo-list-server
+ * @className: GroupPojo
+ * @Description: 组实体类
+ * @version: v1.0
+ * @author: Aye10032
+ * @date: 2022/2/9 下午 9:44
+ */
+
+@Data
+@NoArgsConstructor
+@ApiModel("组信息")
+public class GroupPojo {
+
+    private Integer id;
+
+    @ApiModelProperty("组创建人")
+    @NotNull(message = "group must have owner")
+    private Integer owner;
+
+    @ApiModelProperty("组管理员")
+    private List<Integer> admins;
+
+}
