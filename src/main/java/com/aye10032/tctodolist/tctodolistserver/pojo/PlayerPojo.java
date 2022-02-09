f84498bde51c00707369380ae0241e84c5557c32
Aye10032 <aye10032@gmail.com>
2022-02-09 21:43:49 +0800

    玩家实体类

--- src/main/java/com/aye10032/tctodolist/tctodolistserver/pojo/PlayerPojo.java
+++ src/main/java/com/aye10032/tctodolist/tctodolistserver/pojo/PlayerPojo.java
@@ -1,5 +1,13 @@
 package com.aye10032.tctodolist.tctodolistserver.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 /**
  * @program: tc-todo-list-server
  * @className: PlayerPojo
@@ -8,5 +16,27 @@ package com.aye10032.tctodolist.tctodolistserver.pojo;
  * @author: Aye10032
  * @date: 2022/2/9 下午 5:46
  */
+
+@Data
+@NoArgsConstructor
+@ApiModel("玩家信息")
 public class PlayerPojo {
+
+    private Integer id;
+
+    @ApiModelProperty("玩家ID")
+    @NotNull(message = "player must have name")
+    private String name;
+
+    @ApiModelProperty("玩家UUID")
+    @NotNull(message = "player must have uuid")
+    private String uuid;
+
+    @ApiModelProperty("玩家是否是管理员")
+    @NotNull(message = "must be told if player is admin")
+    private Boolean op;
+
+    @ApiModelProperty("玩家所属组")
+    private List<Integer> groups;
+
 }
