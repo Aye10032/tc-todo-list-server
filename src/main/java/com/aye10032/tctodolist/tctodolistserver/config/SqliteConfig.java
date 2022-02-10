09e36c4e9f7eb1f7850d10df5b7fadb1c88b266c
Aye10032 <aye10032@gmail.com>
2022-02-10 15:09:27 +0800

    group初始化

--- src/main/java/com/aye10032/tctodolist/tctodolistserver/config/SqliteConfig.java
+++ src/main/java/com/aye10032/tctodolist/tctodolistserver/config/SqliteConfig.java
@@ -1,6 +1,7 @@
 package com.aye10032.tctodolist.tctodolistserver.config;
 
 import com.aye10032.tctodolist.tctodolistserver.dao.*;
+import com.aye10032.tctodolist.tctodolistserver.pojo.GroupPojo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Dazo66
@@ -40,11 +43,14 @@ public class SqliteConfig {
             playerDao.createPlayerTable();
             todoDao.CreateTodoTable();
             undertakeDao.CreateUndertakeTable();
+
+            GroupInit();
             log.info("表初始化成功");
         }else if (!StringUtils.isEmpty(sqliteUrl)
                 && new File(sqliteUrl.replace("jdbc:sqlite:", "")).exists()){
             if (groupDao.GroupTableExist() == 0){
                 groupDao.CreateGroupTable();
+                GroupInit();
             }
             if (messageDao.MessageTableExist() == 0){
                 messageDao.CreateMessageTable();
@@ -60,6 +66,19 @@ public class SqliteConfig {
             }
             log.info("表加载成功");
         }
+    }
+
+    //创建服务器默认组
+    private void GroupInit(){
+        GroupPojo group = new GroupPojo();
+
+        //没有创建者
+        group.setOwner(-1);
+
+        List<Integer> admins = new ArrayList<>();
+        group.setAdmins(admins);
+
+        groupDao.insertGroup(group);
     }
 
 }
